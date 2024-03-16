253a6a1e891138e24604598367ca4cde2cff95fb
Iommy <VunixNetwork@gmail.com>
2024-03-16 20:12:59 +0300

    commit

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+document.addEventListener('mousemove', function(e){
+    var customCursor = document.querySelector('.custom-cursor');
+    customCursor.style.left = e.pageX + 'px';
+    customCursor.style.top = e.pageY + 'px';
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App/>
+        <div className="custom-cursor"></div>
+    </React.StrictMode>,
 )
